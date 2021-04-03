94ebd0eeb971494645f92fa76b0d6bdf45f6e108
Beksultan Tatenov <bekatatenov@gmail.com>
2021-04-03 11:28:52 +0600

    add empty Preload Data

--- src/main/java/com/exam9/exam9/service/UserService.java
+++ src/main/java/com/exam9/exam9/service/UserService.java
@@ -1,11 +1,25 @@
 package com.exam9.exam9.service;
 
+import com.exam9.exam9.repository.CommentRepository;
+import com.exam9.exam9.repository.ThemeRepository;
 import com.exam9.exam9.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository userRepository,
+                                   ThemeRepository themeRepository,
+                                   CommentRepository commentRepository) {
+        return (args) -> {
+
+        };
+    }
 }
